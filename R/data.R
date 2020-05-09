9229777edaac15e8a8015f3f9db3a7bb4548796a
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-09 23:35:23 +0200

    Modified and added documentation

--- R/data.R
+++ R/data.R
@@ -9,7 +9,7 @@
 #'
 #' @usage data(elec_price)
 #'
-#' @format A time series object of class \code{tsibble} with 43.800 rows and 3 variables:
+#' @format A time series object of class \code{tsibble} with 43.800 rows and 3 columns:
 #'    \itemize{
 #'       \item{date_time: Date and time (index variable)}
 #'       \item{variable: Variable name (key variable)}
@@ -23,3 +23,39 @@
 #' @examples
 #' data(elec_price)
 "elec_price"
+
+
+
+
+#' @title Daily Sales Data from the M5-Forecasting Competition
+#'
+#' @description Daily "tsibble" with sales data from the M5-Forecasting
+#'    Competition. The dataset contains time series data
+#'    from 2011-01-29 to 2016-04-24. The sales data are aggregated for
+#'    category (cat_id), stores (store_id) and states (state_id).
+#'
+#' @docType data
+#'
+#' @usage data(m5_data)
+#'
+#' @format A time series object of class \code{tsibble} with 172.170 rows and 10 columns:
+#'    \itemize{
+#'       \item{variable: Unique identifier for category, store and state (key variable)}
+#'       \item{cat_id: Product category (Hobbies, Foods, Household)}
+#'       \item{store_id: Store number}
+#'       \item{state_id: State (CA, TX, WI)}
+#'       \item{date: Date (index variable)}
+#'       \item{year: Year}
+#'       \item{month: Month}
+#'       \item{day: Day}
+#'       \item{wday: Weekday}
+#'       \item{value: Sales data (measurement variable)}
+#'       }
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://www.kaggle.com/c/m5-forecasting-accuracy/data}{M5-Forecasting - Accuracy}
+#'
+#' @examples
+#' data(m5_data)
+"m5_data"
