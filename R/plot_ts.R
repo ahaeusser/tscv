73868de753b82d88742570cd9bf790ec325027ba
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-10 14:31:59 +0200

    Modified

--- R/plot_ts.R
+++ R/plot_ts.R
@@ -1,10 +1,11 @@
+
 #' @title Plot time series as line chart.
 #'
-#' @description Line chart of one or more time series incl. smooth curve (linear or loess).
+#' @description Plot one or more time series incl. smooth curve (linear or loess) as line chart.
 #'
 #' @param data A tsibble containing the columns time, variable and value.
-#' @param smooth_method Character value defining the smoothing method ("loess", "lm" or NULL).
-#' @param smooth_se Logical value. If TRUE, confidence interval is displayed around smooth.
+#' @param smooth_method Character value. The smoothing method (\code{"loess"}, \code{"lm"} or \code{NULL}).
+#' @param smooth_se Logical value. If \code{TRUE}, confidence interval is displayed around smooth.
 #' @param title Title of the plot.
 #' @param subtitle Subtitle of the plot.
 #' @param xlab Label for the x-axis.
@@ -18,7 +19,8 @@
 #' @param smooth_color Character value defining the line color (smooth curve).
 #' @param smooth_fill Character value defining the fill color of confidence band.
 #' @param smooth_alpha Numeric value defining the transparency of confidence band.
-#' @param base_size Integer value. Base font size.
+#' @param theme_ggplot2 A complete ggplot2 theme.
+#' @param theme_config A list with further arguments passed to \code{ggplot2::theme()}.
 #'
 #' @return p An object of class ggplot.
 #' @export
@@ -40,13 +42,19 @@ plot_ts <- function(data,
                     line_alpha = 1,
                     smooth_fill = "#FDE725FF",
                     smooth_alpha = 0.25,
-                    base_size = 10) {
+                    theme_ggplot2 = theme_gray(),
+                    theme_config = list()) {
+
+  date_time <- index(data)
+  variable <- key_vars(data)
+  value <- measured_vars(data)
 
   # Create ggplot
   p <- ggplot(
     data = data,
-    aes(x = date_time,
-        y = value))
+    aes(
+      x = !!date_time,
+      y = !!sym(value)))
 
   # Create lines
   p <- p + geom_line(
@@ -68,19 +76,23 @@ plot_ts <- function(data,
   }
 
   # Create grid
-  p <- p + facet_wrap(~variable, scales = "free")
+  p <- p + facet_wrap(
+    vars(!!sym(variable)),
+    scales = "free")
 
   # Axis scaling
   p <- p + scale_y_continuous()
+
   # Adjust annotations
   p <- p + labs(title = title)
   p <- p + labs(subtitle = subtitle)
   p <- p + labs(x = xlab)
   p <- p + labs(y = ylab)
   p <- p + labs(caption = caption)
-  # Adjust theme
-  p <- p + theme_tscv(base_size = base_size)
-  p <- p + theme(legend.position = "none")
+
+  # Adjust ggplot2 theme
+  p <- p + eval(theme_ggplot2)
+  p <- p + do.call(theme, theme_config)
   return(p)
 
 }
