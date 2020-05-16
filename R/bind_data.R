fcd95cafa51ba823832f7c409a8eb87d359ab7d3
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-16 21:41:44 +0200

    Modified due to tidyeval

--- R/bind_data.R
+++ R/bind_data.R
@@ -3,7 +3,7 @@
 #'
 #' @description Bind data and forecasts row-wise into one tsibble. The key structure of both objects is updated.
 #'
-#' @param data A tsibble containing the data provide by \code{split_data(...)}.
+#' @param data A tsibble containing the data provided by \code{split_data(...)}.
 #' @param fcsts A fable ('forecast table') provided by \code{forecast_model(...)}.
 #'
 #' @return data A tsibble containing the input data and the forecasts.
@@ -11,19 +11,22 @@
 
 bind_data <- function(data,
                       fcsts) {
+
+  variable <- key_vars(data)
+
   data <- data %>%
     update_tsibble(
-      key = c(variable,
-              slice,
-              model,
-              type))
+      key = c(
+        !!!syms(variable),
+        model,
+        type))
 
   fcsts <- fcsts %>%
     update_tsibble(
-      key = c(variable,
-              slice,
-              model,
-              type))
+      key = c(
+        !!!syms(variable),
+        model,
+        type))
 
   data <- rbind(data, fcsts)
   return(data)
