8cf888da6e821c4970bcaa4866524ea7da0f0942
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-16 21:42:58 +0200

    Modified due to tidyeval

--- R/split_data.R
+++ R/split_data.R
@@ -7,7 +7,7 @@
 #'    for training and testing via \code{n_init} and \code{n_ahead}, as well as the step size for increments via
 #'    \code{n_step}.
 #'
-#' @param data A tsibble in long format with standard columns (date_time, variable and value) provided by the function \code{clean_data(...)}.
+#' @param data A valid tsibble, either in long format provided by the function \code{clean_data(...)}.
 #' @param mode Character value. Define the setup of the training window for time series cross validation. \code{stretch} is equivalent to an expanding window approach and \code{slide} is a fixed window approach.
 #' @param n_init Integer value. The number of periods for the initial training window (must be positive).
 #' @param n_step Integer value. The number of periods to skip between windows (must be positive).
@@ -15,10 +15,11 @@
 #'
 #' @return data A tsibble with the same format like the input data, but with additional columns:
 #'    \itemize{
-#'       \item{\code{slice}: Integer value. The number of the time slice (training and testing).}
 #'       \item{\code{sample}: Character value. Indicating whether the partition is training or testing.}
+#'       \item{\code{slice}: Integer value. The number of the time slice (training and testing).}
 #'       \item{\code{horizon}: Integer value. The forecast horizon (i.e. the size of the testing window. NA for training.}
-#'       \item{\code{id}: Integer value. The number of the observations within training and testing.}
+#'       \item{\code{type}: Character value. Indicating actual values, forecasts etc.}
+#'       \item{\code{model}: Character value. The forecasting model.}
 #'       }
 #' @export
 
@@ -28,27 +29,36 @@ split_data <- function(data,
                        n_step,
                        n_ahead) {
 
+  date_time <- index_var(data)
+  variable <- key_vars(data)
+  value <- measured_vars(data)
+
+  # End date of the sample
+  end_train <- data %>%
+    pull(!!sym(date_time)) %>%
+    unique() %>%
+    last()
+
   # Extend data for out-of-sample forecasts
   # (measurement variables are set to NA)
   data <- data %>%
     update_tsibble(
-      index = date_time,
-      key = c(variable)) %>%
+      index = !!sym(date_time),
+      key = c(!!!syms(variable))) %>%
     append_row(n = n_ahead) %>%
     mutate(type = "actual") %>%
     mutate(model = NA_character_)
 
-  # Get start date for the first forecast
-  start_date <- data %>%
+  # Start date of first forecast (test slice one)
+  start_test <- data %>%
     filter(row_number() == (n_init + 1)) %>%
-    pull(date_time) %>%
-    as.character()
+    pull(!!sym(date_time))
 
   # Prepare training data .....................................................
 
   # Remove (enhanced) NAs
   data_train <- data %>%
-    filter(!is.na(value))
+    filter_index(. ~ end_train)
 
   # Roll up tsibble by tscv mode
   if (mode == "stretch") {
@@ -76,13 +86,13 @@ split_data <- function(data,
 
   # Roll up tsibble
   data_test <- data %>%
-    filter_index(start_date ~ .) %>%
+    filter_index(start_test ~ .) %>%
     slide_tsibble(
       .size = n_ahead,
       .step = n_step,
       .id = "slice") %>%
     mutate(sample = "test") %>%
-    group_by(slice, variable) %>%
+    group_by(slice, !!!syms(variable)) %>%
     mutate(horizon = row_number()) %>%
     ungroup()
 
@@ -91,19 +101,15 @@ split_data <- function(data,
 
   # Adjust key variables
   data <- data %>%
-    update_tsibble(
-      index = date_time,
-      key = c(variable,
-              slice)) %>%
     select(
-      date_time,
-      variable,
+      !!sym(date_time),
+      !!!syms(variable),
       sample,
       slice,
       horizon,
       type,
       model,
-      value)
+      !!sym(value))
 
   return(data)
 }
