ee09d871a05daa7fb12c6cc182ec1c6140209037
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-24 16:27:15 +0200

    Added start, end, skewness and kurtosis, Removed histogram column

--- R/summarise_data.R
+++ R/summarise_data.R
@@ -2,19 +2,22 @@
 #' @title Summary statistics for time series data.
 #'
 #' @description This function calculates several summary statistics for time series data.
-#'    The function calculates the following statistics for all key combinations:
+#'    The function calculates the following metrics for all key combinations:
 #'
 #'    \itemize{
+#'       \item{\code{start}: Start date (or date-time)}
+#'       \item{\code{end}: End date (or date-time)}
 #'       \item{\code{n_missing}: Number of missing values (NAs)}
 #'       \item{\code{complete_rate}: Percentange rate of complete values}
-#'       \item{\code{mean}: Mean}
-#'       \item{\code{sd}: Standard Deviation}
+#'       \item{\code{mean}: Arithmetic mean}
+#'       \item{\code{sd}: Standard deviation}
 #'       \item{\code{p0}: Minimum}
 #'       \item{\code{p25}: 25\%-Quantile}
 #'       \item{\code{p50}: Median}
 #'       \item{\code{p75}: 75\%-Quantile}
 #'       \item{\code{p100}: Maximum}
-#'       \item{\code{hist}: Histogram as sparkline}
+#'       \item{\code{skewness}: Skewness}
+#'       \item{\code{kurtosis}: Kurtosis}
 #'       }
 #'
 #' @param data A valid tsibble in long format with one measurement variable.
@@ -32,42 +35,25 @@ summarise_data <- function(data) {
     abort("Only one measured variable is supported.")
   }
 
-  data <- data %>%
+  data_tbl <- data %>%
     as_tibble() %>%
-    select(-!!sym(date_time)) %>%
-    group_by(!!!syms(variable))
-
-  skim_adj <- skim_with(
-    numeric = sfl(n_obs = length))
-
-  data_tbl <- skim_adj(data = data) %>%
-    as_tibble() %>%
-    select(-c(skim_type, skim_variable)) %>%
-    rename(
-      mean = numeric.mean,
-      sd = numeric.sd,
-      p0 = numeric.p0,
-      p25 = numeric.p25,
-      p50 = numeric.p50,
-      p75 = numeric.p75,
-      p100 = numeric.p100,
-      hist = numeric.hist,
-      n_obs = numeric.n_obs)
-
-  data_tbl <- data_tbl %>%
-    select(
-      c(!!!syms(variable)),
-      n_obs,
-      n_missing,
-      complete_rate,
-      mean,
-      sd,
-      p0,
-      p25,
-      p50,
-      p75,
-      p100,
-      hist)
+    group_by(!!!syms(variable)) %>%
+    summarise(
+      start = first(!!sym(date_time)),
+      end = last(!!sym(date_time)),
+      n_obs = n(),
+      n_missing = sum(is.na(.)),
+      complete_rate = sum(is.na(.)) / n(),
+      mean = mean(!!sym(value), na.rm = TRUE),
+      sd = sd(!!sym(value), na.rm = TRUE),
+      p0 = quantile(!!sym(value), probs = 0, na.rm = TRUE),
+      p25 = quantile(!!sym(value), probs = 0.25, na.rm = TRUE),
+      p50 = quantile(!!sym(value), probs = 0.5, na.rm = TRUE),
+      p75 = quantile(!!sym(value), probs = 0.75, na.rm = TRUE),
+      p100 = quantile(!!sym(value), probs = 1, na.rm = TRUE),
+      skewness = skewness(!!sym(value), na.rm = TRUE),
+      kurtosis = kurtosis(!!sym(value), na.rm = TRUE)) %>%
+    ungroup()
 
   return(data_tbl)
 }
