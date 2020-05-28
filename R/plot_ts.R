21ae9e739e01ca3780cfb42e4e27f98330ee09dc
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-28 18:35:09 +0200

    Modified stat_smooth()

--- R/plot_ts.R
+++ R/plot_ts.R
@@ -4,8 +4,6 @@
 #' @description Plot one or more time series incl. smooth curve (linear or loess) as line chart.
 #'
 #' @param data A valid tsibble in long format with one measurement variable.
-#' @param smooth_method Character value. The smoothing method (\code{"loess"}, \code{"lm"} or \code{NULL}).
-#' @param smooth_se Logical value. If \code{TRUE}, confidence interval is displayed around smooth.
 #' @param title Title of the plot.
 #' @param subtitle Subtitle of the plot.
 #' @param xlab Label for the x-axis.
@@ -14,6 +12,8 @@
 #' @param line_width Numeric value defining the line width (time series).
 #' @param line_type Numeric value defining the line type (time series).
 #' @param line_color Character value defining the line color (time series).
+#' @param smooth_method Character value. The smoothing method (\code{NULL}, \code{"loess"}, \code{"lm"}, \code{glm}, \code{gam}).
+#' @param smooth_se Logical value. If \code{TRUE}, confidence interval is displayed around smooth.
 #' @param smooth_width Numeric value defining the line width (smooth curve).
 #' @param smooth_type Numeric value defining the line type (smooth curve).
 #' @param smooth_color Character value defining the line color (smooth curve).
@@ -21,13 +21,12 @@
 #' @param smooth_alpha Numeric value defining the transparency of confidence band.
 #' @param theme_set A complete ggplot2 theme.
 #' @param theme_config A list with further arguments passed to \code{ggplot2::theme()}.
+#' @param ... Further arguments passed to \code{ggplot2::stat_smooth()}
 #'
 #' @return p An object of class ggplot.
 #' @export
 
 plot_ts <- function(data,
-                    smooth_method = NULL,
-                    smooth_se = FALSE,
                     title = NULL,
                     subtitle = NULL,
                     xlab = NULL,
@@ -40,10 +39,12 @@ plot_ts <- function(data,
                     line_type = "solid",
                     line_color = "#31688EFF",
                     line_alpha = 1,
+                    smooth_method = NULL,
                     smooth_fill = "#D55E00",
                     smooth_alpha = 0.25,
                     theme_set = theme_tscv(),
-                    theme_config = list()) {
+                    theme_config = list(),
+                    ...) {
 
   date_time <- index_var(data)
   variable <- key_vars(data)
@@ -65,14 +66,14 @@ plot_ts <- function(data,
 
   # Create smooth
   if (!is.null(smooth_method)) {
-    p <- p + geom_smooth(
+    p <- p + stat_smooth(
       method = smooth_method,
-      se = smooth_se,
       color = smooth_color,
       fill = smooth_fill,
       size = smooth_width,
       linetype = smooth_type,
-      alpha = smooth_alpha)
+      alpha = smooth_alpha,
+      ...)
   }
 
   # Create grid
@@ -87,7 +88,7 @@ plot_ts <- function(data,
   p <- p + labs(title = title)
   p <- p + labs(subtitle = subtitle)
   p <- p + labs(x = if_else(is_empty(xlab), date_time, xlab))
-  p <- p + labs(y = if_else(is_empty(ylab), value, xlab))
+  p <- p + labs(y = if_else(is_empty(ylab), value, ylab))
   p <- p + labs(caption = caption)
 
   # Adjust ggplot2 theme
