59ac53ca9a2b7a1479e3da16c6cd8c98b42ac6a6
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-28 18:35:36 +0200

    Adjusted fill color and alpha

--- R/plot_season.R
+++ R/plot_season.R
@@ -3,7 +3,7 @@
 #'
 #' @description Seasonal plot of one or more time series.
 #'
-#' @param data A tsibble containing the columns time, variable and value.
+#' @param data A valid tsibble in long format with one measurement variable.
 #' @param title Title of the plot.
 #' @param subtitle Subtitle of the plot.
 #' @param xlab Label for the x-axis.
@@ -29,6 +29,8 @@ plot_season <- function(data,
                         line_type = "solid",
                         line_color = "#31688EFF",
                         line_alpha = 1,
+                        fill_color = "#31688EFF",
+                        fill_alpha = 0.2,
                         theme_set = theme_tscv(),
                         theme_config = list()) {
 
@@ -36,48 +38,70 @@ plot_season <- function(data,
   variable <- key_vars(data)
   value <- measured_vars(data)
 
+  # # Prepare data
+  # data <- data %>%
+  #   mutate(wday = lubridate::wday(!!sym(date_time), label = TRUE, week_start = getOption("lubridate.week.start", 1))) %>%
+  #   mutate(hour = lubridate::hour(!!sym(date_time))) %>%
+  #   as_tibble() %>%
+  #   select(-!!sym(date_time)) %>%
+  #   group_by(!!!syms(variable), wday, hour) %>%
+  #   summarise(
+  #     `10%` = quantile(!!sym(value), probs = 0.10, na.rm = TRUE),
+  #     `25%` = quantile(!!sym(value), probs = 0.25, na.rm = TRUE),
+  #     median = quantile(!!sym(value), probs = 0.5, na.rm = TRUE),
+  #     `75%` = quantile(!!sym(value), probs = 0.75, na.rm = TRUE),
+  #     `90%` = quantile(!!sym(value), probs = 0.90, na.rm = TRUE)) %>%
+  #   ungroup()
+
   # Prepare data
   data <- data %>%
-    mutate(wday = lubridate::wday(date_time, label = TRUE, week_start = getOption("lubridate.week.start", 1))) %>%
-    mutate(hour = lubridate::hour(date_time)) %>%
+    mutate(wday = lubridate::wday(!!sym(date_time), label = TRUE, week_start = getOption("lubridate.week.start", 1))) %>%
+    mutate(hour = lubridate::hour(!!sym(date_time))) %>%
     as_tibble() %>%
-    select(-date_time) %>%
-    group_by(variable, wday, hour) %>%
+    select(-!!sym(date_time)) %>%
+    group_by(!!!syms(variable), wday, hour) %>%
     summarise(
-      `10%` = quantile(value, probs = 0.10),
-      `25%` = quantile(value, probs = 0.25),
-      median = quantile(value, probs = 0.5),
-      `75%` = quantile(value, probs = 0.75),
-      `90%` = quantile(value, probs = 0.90)) %>%
+      median = median(!!sym(value), na.rm = TRUE),
+      mad = mad(!!sym(value), na.rm = TRUE)) %>%
     ungroup()
 
-
   # Create ggplot
-  p <- ggplot2::ggplot(data = data)
+  p <- ggplot(data = data)
 
   p <- p + facet_grid(
     cols = vars(wday),
-    rows = vars(variable),
+    rows = vars(!!!syms(variable)),
     scales = "free")
 
-  p <- p + geom_ribbon(
-    aes(
-      x = hour,
-      ymin = `10%`,
-      ymax = `90%`,
-      fill = "10%-90%"),
-    alpha = 0.2)
+  # p <- p + geom_ribbon(
+  #   aes(
+  #     x = hour,
+  #     ymin = `10%`,
+  #     ymax = `90%`,
+  #     fill = "10%-90%"),
+  #   alpha = 0.2)
+  #
+  # p <- p + geom_ribbon(
+  #   aes(
+  #     x = hour,
+  #     ymin = `25%`,
+  #     ymax = `75%`,
+  #     fill = "25%-75%"),
+  #   alpha = 0.3)
+
+  # p <- p + scale_fill_manual(
+  #   values = c(fill_color, fill_color))
 
   p <- p + geom_ribbon(
     aes(
       x = hour,
-      ymin = `25%`,
-      ymax = `75%`,
-      fill = "25%-75%"),
-    alpha = 0.3)
+      ymin = median - mad,
+      ymax = median + mad,
+      fill = "MAD"),
+    alpha = fill_alpha)
 
   p <- p + scale_fill_manual(
-    values = c("#31688EFF", "#31688EFF"))
+    values = c(fill_color))
 
   p <- p + geom_line(
     aes(
@@ -93,14 +117,13 @@ plot_season <- function(data,
     values = line_color)
 
   p <- p + scale_y_continuous()
-  p <- p + scale_x_continuous(breaks = c(0, 6, 12, 18))
 
   # Adjust annotations
-  p <- p + ggplot2::labs(title = title)
-  p <- p + ggplot2::labs(subtitle = subtitle)
-  p <- p + labs(x = if_else(is_empty(xlab), "Hour", xlab))
+  p <- p + labs(title = title)
+  p <- p + labs(subtitle = subtitle)
+  p <- p + labs(x = if_else(is_empty(xlab), date_time, xlab))
   p <- p + labs(y = if_else(is_empty(ylab), value, ylab))
-  p <- p + ggplot2::labs(caption = caption)
+  p <- p + labs(caption = caption)
 
   # Adjust ggplot2 theme
   p <- p + eval(theme_set)
