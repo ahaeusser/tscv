247a9951185664d26f5d77efbb407e8c09715171
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-29 17:50:53 +0200

    Adjusted documentation

--- R/bind_data.R
+++ R/bind_data.R
@@ -3,8 +3,8 @@
 #'
 #' @description Bind data and forecasts row-wise into one tsibble. The key structure of both objects is updated.
 #'
-#' @param data A tsibble containing the data provided by \code{split_data(...)}.
-#' @param fcsts A fable ('forecast table') provided by \code{forecast_model(...)}.
+#' @param data A tsibble containing the data provided by \code{split_data()}.
+#' @param fcsts A fable ('forecast table') provided by \code{forecast_model()}.
 #'
 #' @return data A tsibble containing the input data and the forecasts.
 #' @export
