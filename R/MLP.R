4816300602bb7b9167e001997b65806703ed8496
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-05-30 02:58:18 +0200

    Added wrapper for nnfor::mlp()

---
+++ R/MLP.R
@@ -0,0 +1,148 @@
+
+#' @title Train MLP neural network.
+#'
+#' @description Train a Multilayer Perceptron (MLP).
+#'
+#' @param .data Input data as \code{tsibble}.
+#' @param specials Specials as list defined in \code{specials_mlp}.
+#' @param ... Further arguments passed to \code{nnfor::mlp()}.
+#'
+#' @return An object of class \code{MLP}.
+
+train_mlp <- function(.data,
+                      specials,
+                      ...){
+
+  if(length(tsibble::measured_vars(.data)) > 1){
+    abort("Only univariate responses are supported by MLP.")
+  }
+
+  # Prepare data for modelling
+  model_data <- as.ts(.data)
+
+  if(any(is.na(model_data))){
+    abort("MLP does not support missing values.")
+  }
+
+  # Train model
+  mdl <- nnfor::mlp(y = model_data, ...)
+
+  # Extract length of actual values and fitted values
+  n_total <- length(mdl$y)
+  n_fitted <- length(mdl$fitted)
+
+  # Fill NAs in front of fitted values (adjust to equal length of actual values)
+  fit <- c(rep(NA_real_, n_total - n_fitted), mdl$fitted)
+  res <- mdl$y - fit
+
+  # Return model
+  structure(
+    list(
+      model = mdl,
+      est = list(
+        .fitted = fit,
+        .resid = res)),
+    class = "MLP")
+}
+
+
+
+specials_mlp <- new_specials()
+
+#' @title Automatic training of MLPs.
+#'
+#' @description Automatic training of MLPs.
+#'
+#' @param formula Model specification (see "Specials" section, currently not in use...)
+#' @param ... Further arguments passed to \code{nnfor::mlp()}.
+#'
+#' @return mlp_model An object of class \code{MLP}.
+#' @export
+
+MLP <- function(formula, ...){
+  mlp_model <- new_model_class(
+    model = "MLP",
+    train = train_mlp,
+    specials = specials_mlp)
+
+  new_model_definition(
+    mlp_model,
+    !!enquo(formula),
+    ...)
+}
+
+
+#' @title Forecast a trained MLP model.
+#'
+#' @description Forecast a trained MLP model.
+#'
+#' @param object The trained MLP model used to produce the forecasts.
+#' @param new_data Forecast horizon.
+#' @param specials Specials are currently not in use.
+#' @param ... Additional arguments for forecast model method.
+#'
+#' @return An object of class "fable".
+#' @export
+
+forecast.MLP <- function(object,
+                         new_data,
+                         specials = NULL,
+                         ...){
+  # Extract model
+  mdl <- object$model
+  # Forecast model
+  fcst <- forecast(mdl, h = nrow(new_data))
+
+  # Extract point forecast and simulations
+  mean <- fcst$mean
+  sim <- fcst$all.mean
+
+  # Return forecasts
+  construct_fc(
+    point = mean,
+    sd = rowSds(sim),
+    dist = dist_normal(mean = mean, sd = rowSds(sim)))
+}
+
+
+#' @title Extract fitted values from a MLP.
+#'
+#' @description Extract fitted values from a MLP.
+#'
+#' @param object The time series model used to produce the forecast.
+#' @param ... Further arguments.
+#'
+#' @return
+#' @export
+
+fitted.MLP <- function(object, ...){
+  object$est[[".fitted"]]
+}
+
+
+#' @title Extract residuals from a MLP.
+#'
+#' @description Extract residuals from a MLP.
+#'
+#' @param object The time series model used to produce the forecast.
+#' @param ... Further arguments.
+#'
+#' @return
+#' @export
+
+residuals.MLP <- function(object, ...){
+  object$est[[".resid"]]
+}
+
+#' @title Provide a succinct summary of the MLP.
+#'
+#' @description Provide a succinct summary of the MLP.
+#'
+#' @param object The MLP to summarize.
+#'
+#' @return
+#' @export
+
+model_sum.MLP <- function(x){
+  "MLP"
+}
