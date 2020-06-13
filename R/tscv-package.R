bed1d72c484ca15fc98d08a91d21064b5811560d
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-06-13 16:07:51 +0200

    Fixed NAMESPACE and DESCRIPTION

--- R/tscv-package.R
+++ R/tscv-package.R
@@ -1,18 +1,20 @@
 
 #' @importFrom dplyr filter as_tibble group_by left_join pull select_if mutate n n_distinct rename slice summarise
-#' @importFrom dplyr ungroup select bind_rows arrange group_by_if row_number distinct do full_join desc last
+#' @importFrom dplyr ungroup select bind_rows arrange group_by_if row_number distinct do full_join desc first last if_else starts_with
 #' @importFrom tidyr fill pivot_longer pivot_wider drop_na gather spread
-#' @importFrom tibble add_column
-#' @importFrom lubridate year month hours
-#' @importFrom rlang is_empty quo_is_null as_name
+#' @importFrom tibble add_column add_row
+#' @importFrom lubridate year quarter month hours
+#' @importFrom rlang is_empty quo_is_null as_name abort .data
 #' @importFrom future plan
 #' @importFrom magrittr "%>%"
-#' @importFrom feasts ACF PACF
+#' @importFrom feasts ACF PACF STL
 #' @importFrom nnfor elm mlp
 #' @importFrom forecast msts dshw tbats thetaf forecast
 #' @importFrom matrixStats rowSds
 #' @importFrom yardstick mae_vec rmse_vec mape_vec smape_vec mase_vec metric_vec_template metric_summarizer
 #' @importFrom scales pretty_breaks
+#' @importFrom stats approx quantile qnorm median mad sd as.ts acf pacf lag time
+#' @importFrom grDevices colorRampPalette
 #' @rawNamespace import(ggplot2, except = stat_qq_line)
 #' @importFrom qqplotr stat_qq_point stat_qq_line stat_qq_band
 #' @import tsibble
