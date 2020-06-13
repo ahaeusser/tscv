7cd4fe69f083b297377450a1d97767dc3bb201c9
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-06-13 12:20:09 +0200

    updated

--- R/tscv-package.R
+++ R/tscv-package.R
@@ -3,17 +3,16 @@
 #' @importFrom dplyr ungroup select bind_rows arrange group_by_if row_number distinct do full_join desc last
 #' @importFrom tidyr fill pivot_longer pivot_wider drop_na gather spread
 #' @importFrom tibble add_column
-#' @importFrom readr read_csv2
 #' @importFrom lubridate year month hours
-#' @importFrom rlang is_empty
+#' @importFrom rlang is_empty quo_is_null as_name
 #' @importFrom future plan
 #' @importFrom magrittr "%>%"
 #' @importFrom feasts ACF PACF
-#' @importFrom moments skewness kurtosis
 #' @importFrom nnfor elm mlp
 #' @importFrom forecast msts dshw tbats thetaf forecast
 #' @importFrom matrixStats rowSds
 #' @importFrom yardstick mae_vec rmse_vec mape_vec smape_vec mase_vec metric_vec_template metric_summarizer
+#' @importFrom scales pretty_breaks
 #' @rawNamespace import(ggplot2, except = stat_qq_line)
 #' @importFrom qqplotr stat_qq_point stat_qq_line stat_qq_band
 #' @import tsibble
