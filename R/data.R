d68568900167577de2d6c443e41e9866d0cf1f48
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-10-16 09:35:56 +0200

    Updated documentation

--- R/data.R
+++ R/data.R
@@ -2,17 +2,18 @@
 #'
 #' @description Hourly \code{tsibble} with day-ahead electricity spot prices from
 #'    the ENTSO-E Transparency Platform. The dataset contains time series data
-#'    from 2019-01-01 00:00:00 to 2019-12-31 23:00:00 for four bidding zones
-#'    within europe (DE, FR, SE1, NO1).
+#'    from 2019-01-01 00:00:00 to 2019-12-31 23:00:00 for 8 bidding zones
+#'    within europe (DE, DK1, ES, FI, FR, NL, NO1, SE1).
 #'
 #' @docType data
 #'
 #' @usage data(elec_price)
 #'
-#' @format A time series object of class \code{tsibble} with 35.040 rows and 4 columns:
+#' @format A time series object of class \code{tsibble} with 70.080 rows and 5 columns:
 #'    \itemize{
 #'       \item{\code{Time}: Date and time (index variable)}
 #'       \item{\code{Series}: Time series name (key variable)}
+#'       \item{\code{Unit}: Measured unit (key variable)}
 #'       \item{\code{BZN}: Bidding zone (key variable)}
 #'       \item{\code{Value}: Measurement variable}
 #'       }
@@ -30,19 +31,21 @@
 
 #' @title Hourly electricity load
 #'
-#' @description Hourly \code{tsibble} with electricity loads from
-#'    the ENTSO-E Transparency Platform. The dataset contains time series data
-#'    from 2019-01-01 00:00:00 to 2019-12-31 23:00:00 for four bidding zones
-#'    within europe (DE, FR, SE1, NO1).
+#' @description Hourly \code{tsibble} with electricity loads and load forecasts
+#'    from the ENTSO-E Transparency Platform. The dataset contains time series data
+#'    from 2019-01-01 00:00:00 to 2019-12-31 23:00:00 for 8 bidding zones within
+#'    europe (DE, DK1, ES, FI, FR, NL, NO1, SE1). The data are aggregated in time
+#'    from a 15-minutes interval to an hourly basis.
 #'
 #' @docType data
 #'
 #' @usage data(elec_load)
 #'
-#' @format A time series object of class \code{tsibble} with 35.040 rows and 4 columns:
+#' @format A time series object of class \code{tsibble} with 122.640 rows and 5 columns:
 #'    \itemize{
 #'       \item{\code{Time}: Date and time (index variable)}
 #'       \item{\code{Series}: Time series name (key variable)}
+#'       \item{\code{Unit}: Measured unit (key variable)}
 #'       \item{\code{BZN}: Bidding zone (key variable)}
 #'       \item{\code{Value}: Measurement variable}
 #'       }
