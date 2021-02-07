2a74562ffd0f0a9378015762dec526f48fb1b6ee
Alexander Haeusser <alexander-haeusser@gmx.de>
2021-02-07 15:47:40 +0100

    Modified function and documentation (removed geom_smooth)

--- R/plot_line.R
+++ R/plot_line.R
@@ -1,31 +1,26 @@
 
-#' @title Plot time series as line chart
+#' @title Plot data as line chart
 #'
-#' @description Plot one or more time series incl. smooth curve (linear or loess) as line chart.
+#' @description Plot one or more variables as line chart.
 #'
 #' @param data A \code{data.frame}, \code{tibble} or \code{tsibble} in long format.
 #' @param x Unquoted column within \code{.data}.
 #' @param y Unquoted column within \code{.data} containing numeric values.
-#' @param facet Unquoted column within \code{.data} (facet).
+#' @param facet_var Unquoted column within \code{.data} (facet).
+#' @param facet_scale Character value defining axis scaling (\code{facet_var = "free"} or \code{facet_var = "fixed"}).
 #' @param color Unquoted column within \code{.data} (color).
 #' @param title Title of the plot.
 #' @param subtitle Subtitle of the plot.
 #' @param xlab Label for the x-axis.
 #' @param ylab Label for the y-axis.
 #' @param caption Caption of the plot.
-#' @param line_width Numeric value defining the line width (time series).
-#' @param line_type Numeric value defining the line type (time series).
+#' @param line_size Numeric value defining the line width.
+#' @param line_type Numeric value defining the line type.
 #' @param line_color Character value defining the line color (ignored if \code{color} is present).
 #' @param line_alpha Numeric value defining the transparency of the line.
-#' @param stat_method Character value. The smoothing method (\code{NULL}, \code{"loess"}, \code{"lm"}, \code{"glm"}, \code{"gam"}).
-#' @param stat_width Numeric value defining the line width (smooth curve).
-#' @param stat_type Numeric value defining the line type (smooth curve).
-#' @param stat_color Character value defining the line color (ignored if \code{color} is present).
-#' @param stat_fill Character value defining the fill color of confidence band.
-#' @param stat_alpha Numeric value defining the transparency of confidence band.
 #' @param theme_set A complete ggplot2 theme.
 #' @param theme_config A list with further arguments passed to \code{ggplot2::theme()}.
-#' @param ... Further arguments passed to \code{ggplot2::stat_smooth()}
+#' @param ... Currently not in use.
 #'
 #' @return p An object of class ggplot.
 #' @export
@@ -33,23 +28,18 @@
 plot_line <- function(data,
                       x,
                       y,
-                      facet = NULL,
+                      facet_var = NULL,
+                      facet_scale = "free",
                       color = NULL,
                       title = NULL,
                       subtitle = NULL,
                       xlab = NULL,
                       ylab = NULL,
                       caption = NULL,
-                      line_width = 0.75,
+                      line_size = 0.75,
                       line_type = "solid",
                       line_color = "grey35",
                       line_alpha = 1,
-                      stat_method = NULL,
-                      stat_width = 0.75,
-                      stat_type = "solid",
-                      stat_color = "#D55E00",
-                      stat_fill = "grey35",
-                      stat_alpha = 0.25,
                       theme_set = theme_tscv(),
                       theme_config = list(),
                       ...) {
@@ -64,7 +54,7 @@ plot_line <- function(data,
         x = {{x}},
         y = {{y}}),
       color = line_color,
-      size = line_width,
+      size = line_size,
       linetype = line_type,
       alpha = line_alpha
     )
@@ -74,46 +64,17 @@ plot_line <- function(data,
         x = {{x}},
         y = {{y}},
         color = {{color}}),
-      size = line_width,
+      size = line_size,
       linetype = line_type,
       alpha = line_alpha
     )
   }
 
-  # Create smooth
-  if (!is_empty(stat_method)) {
-    if (quo_is_null(enquo(color))) {
-      p <- p + geom_smooth(
-        aes(
-          x = {{x}},
-          y = {{y}}),
-        method = stat_method,
-        color = stat_color,
-        size = stat_width,
-        linetype = stat_type,
-        alpha = stat_alpha,
-        ...
-      )
-    } else {
-      p <- p + geom_smooth(
-        aes(
-          x = {{x}},
-          y = {{y}},
-          color = {{color}}),
-        method = stat_method,
-        size = stat_width,
-        linetype = stat_type,
-        alpha = stat_alpha,
-        ...
-      )
-    }
-  }
-
   # Create facet
-  if (!quo_is_null(enquo(facet))) {
+  if (!quo_is_null(enquo(facet_var))) {
     p <- p + facet_wrap(
-      vars({{facet}}),
-      scales = "free"
+      vars({{facet_var}}),
+      scales = facet_scale
     )
   }
 
