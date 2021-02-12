7c4bdf75e98d44be0711ddab877516eb4100ee83
Alexander Haeusser <alexander-haeusser@gmx.de>
2021-02-12 08:32:55 +0100

    Modified faceting

--- R/plot_qq.R
+++ R/plot_qq.R
@@ -5,7 +5,10 @@
 #'
 #' @param data A \code{data.frame}, \code{tibble} or \code{tsibble} in long format.
 #' @param x Unquoted column within \code{.data} containing numeric values.
-#' @param facet Unquoted column within \code{.data} (facet).
+#' @param facet_var Unquoted column within \code{.data} (facet).
+#' @param facet_scale Character value defining axis scaling (\code{facet_var = "free"} or \code{facet_var = "fixed"}).
+#' @param facet_nrow Integer value. The number of rows.
+#' @param facet_ncol Integer value. The number of columns.
 #' @param color Unquoted column within \code{.data} (color).
 #' @param title Title of the plot.
 #' @param subtitle Subtitle of the plot.
@@ -33,7 +36,10 @@
 
 plot_qq <- function(data,
                     x,
-                    facet = NULL,
+                    facet_var = NULL,
+                    facet_scale = "free",
+                    facet_nrow = NULL,
+                    facet_ncol = NULL,
                     color = NULL,
                     title = NULL,
                     subtitle = NULL,
@@ -119,10 +125,12 @@ plot_qq <- function(data,
   }
 
   # Create facet
-  if (!quo_is_null(enquo(facet))) {
+  if (!quo_is_null(enquo(facet_var))) {
     p <- p + facet_wrap(
-      vars({{facet}}),
-      scales = "free"
+      facets = vars({{facet_var}}),
+      scales = facet_scale,
+      nrow = facet_nrow,
+      ncol = facet_ncol
     )
   }
 
