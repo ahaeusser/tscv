7868ff27d89f61884145bfb44cce15e49fecba02
Alexander Haeusser <alexander-haeusser@gmx.de>
2021-06-13 13:51:59 +0200

    New imports and exports due to EXPERT model

--- R/tscv-package.R
+++ R/tscv-package.R
@@ -1,9 +1,9 @@
 
-#' @importFrom dplyr filter as_tibble group_by left_join pull select_if mutate n n_distinct rename slice slice_tail summarise all_of
-#' @importFrom dplyr ungroup select bind_rows arrange group_by_if row_number distinct do full_join desc first last if_else starts_with
+#' @importFrom dplyr filter as_tibble group_by group_split left_join pull select_if mutate n n_distinct rename slice slice_tail summarise all_of
+#' @importFrom dplyr ungroup select bind_rows arrange group_by_if row_number distinct do full_join desc first last if_else starts_with lag
 #' @importFrom tidyr fill pivot_longer pivot_wider drop_na gather spread
 #' @importFrom tibble add_column add_row
-#' @importFrom lubridate year quarter month hours wday
+#' @importFrom lubridate year quarter month hour wday date
 #' @importFrom rlang is_empty quo_is_null as_name abort .data ":="
 #' @importFrom future plan
 #' @importFrom magrittr "%>%"
@@ -13,7 +13,7 @@
 #' @importFrom forecast msts dshw tbats thetaf forecast na.interp
 #' @importFrom matrixStats rowSds
 #' @importFrom scales pretty_breaks
-#' @importFrom stats approx quantile qnorm median mad sd as.ts acf pacf lag time density
+#' @importFrom stats approx quantile qnorm median mad sd as.ts acf pacf time density lm
 #' @importFrom grDevices colorRampPalette
 #' @importFrom tidytext scale_x_reordered
 #' @rawNamespace import(ggplot2, except = stat_qq_line)
