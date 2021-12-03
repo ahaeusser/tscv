1070bf2c98a66bc46bef165a0bf6a90352182814
Alexander Haeusser <alexander-haeusser@gmx.de>
2021-12-03 11:18:37 +0100

    New imports and exports, exception for fabletools::forecast()

--- R/tscv-package.R
+++ R/tscv-package.R
@@ -3,6 +3,7 @@
 #' @importFrom dplyr ungroup select bind_rows arrange group_by_if row_number distinct do full_join desc first last if_else starts_with lag
 #' @importFrom tidyr fill pivot_longer pivot_wider drop_na gather spread
 #' @importFrom tibble add_column add_row
+#' @importFrom slider slide_dbl
 #' @importFrom lubridate year quarter month hour wday date
 #' @importFrom rlang is_empty quo_is_null as_name abort .data ":="
 #' @importFrom future plan
@@ -10,7 +11,7 @@
 #' @importFrom feasts ACF PACF STL
 #' @importFrom distributional dist_normal
 #' @importFrom nnfor elm mlp
-#' @importFrom forecast msts dshw tbats thetaf forecast na.interp
+#' @importFrom forecast msts dshw tbats forecast na.interp
 #' @importFrom matrixStats rowSds
 #' @importFrom scales pretty_breaks
 #' @importFrom stats approx quantile qnorm median mad sd as.ts acf pacf time density lm
@@ -20,7 +21,7 @@
 #' @importFrom qqplotr stat_qq_point stat_qq_line stat_qq_band
 #' @import tsibble
 #' @import fable
-#' @import fabletools
+#' @rawNamespace import(fabletools, except = forecast)
 #' @import purrr
 globalVariables(".")
 NULL
